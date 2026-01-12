b15efcc71f280c70ff60dac5604436870fc10649
parthiv39731 <70740707+parthiv39731@users.noreply.github.com>
2026-01-12 18:47:11 +0530

    commit mcp client and server (#37) (#38)

---
+++ springboot-modules/spring-ai-mcp/mcp-client/src/main/java/com/ks/mcp/McpSpringClientApplication.java
@@ -0,0 +1,13 @@
+package com.ks.mcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpSpringClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpSpringClientApplication.class, args);
+	}
+
+}
